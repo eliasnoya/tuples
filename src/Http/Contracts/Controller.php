be001980582f4e1bd17fddc61048d721a3aae3e0
eliasnoya <68211494+eliasnoya@users.noreply.github.com>
2023-11-23 20:45:53 -0300

    Remove dbPool as inject property to baseController

--- src/Http/Contracts/Controller.php
+++ src/Http/Contracts/Controller.php
@@ -2,19 +2,12 @@
 
 namespace Tuples\Http\Contracts;
 
-use Tuples\Database\Database;
 use Tuples\Http\Request;
 use Tuples\Http\Response;
-use Tuples\Database\DatabasePool;
 
 abstract class Controller
 {
-    public function __construct(protected Request $req, protected Response $res, protected DatabasePool $dbPool)
+    public function __construct(protected Request $req, protected Response $res)
     {
-    }
-
-    public function db($connection = 'default'): Database
-    {
-        return $this->dbPool->get($connection);
     }
 }
